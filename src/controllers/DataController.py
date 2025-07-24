e40837c5e42b9cac638a484289e21f575b17bb0e
Abdelrahman Mohamed Ali <102557725+AbdoAlshoki2@users.noreply.github.com>
2025-07-24 14:03:39 +0300

    Add project file upload handling and utilities
    Introduces ProjectController for managing project directories and updates DataController with file name sanitization and unique naming. Enhances the data upload route to save files asynchronously, handle errors, and use chunked writing. Updates environment and config to support default chunk size, adds aiofiles dependency, and includes a .gitignore for asset files.

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,6 +1,9 @@
 from .BaseController import BaseController 
+from .ProjectController import ProjectController
 from fastapi import UploadFile
 from models import ResponseSignal
+import os
+import re
 
 class DataController(BaseController):
 
@@ -16,3 +19,32 @@ class DataController(BaseController):
             return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
 
         return True, ResponseSignal.FILE_UPLOADED_SUCCESS.value
+
+    def generate_unique_file_name(self, org_file_name: str, project_id: str):
+
+        random_key= self.generate_random_string()  # generate random prefix
+        project_path = ProjectController().get_project_dir(project_id=project_id)
+        
+        cleaned_file_name = self.get_clean_file_name(org_file_name=org_file_name)  # clean the original file name
+
+        new_path = os.path.join(project_path , random_key + "_" + cleaned_file_name)
+
+        while os.path.exists(new_path): # if the file already exists, generate a new random prefix
+            random_key= self.generate_random_string()
+            new_path = os.path.join(project_path , random_key + "_" + cleaned_file_name)
+
+        return random_key + "_" + cleaned_file_name
+
+
+    def get_clean_file_name(self, org_file_name: str):
+
+        # remove any special characters, except underscore and .
+        cleaned_file_name = re.sub(r'[^\w.]', '', org_file_name.strip())
+
+        # replace spaces with underscore
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+
+        return cleaned_file_name
+        
+
+        
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Setting
-from controllers import DataController
+from controllers import DataController, ProjectController
+from models import ResponseSignal
+import os
+import aiofiles
+import logging
 
+
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -12,6 +19,41 @@ data_router = APIRouter(
 @data_router.post("/upload/{project_id}")
 async def upload_data(project_id: str, file: UploadFile, app_settings: Setting = Depends(get_settings)):
     
-    is_valid , signal = DataController().validate_uploaded_file(file=file)
+    data_controller = DataController()
+    is_valid , signal = data_controller.validate_uploaded_file(file=file)
 
-    return {"success": is_valid, "signal": signal}
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+             content={
+                "signal": signal
+                }
+        )
+
+    project_dir_path = ProjectController().get_project_dir(project_id=project_id)
+    file_name = data_controller.generate_unique_file_name(org_file_name=file.filename, project_id=project_id)
+
+    file_path = os.path.join(project_dir_path , file_name)
+
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+
+    except Exception as e:
+        
+        logger.error(f"File upload failed: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content={
+                "signal": ResponseSignal.FILE_UPLOADED_FAILED.value
+                }
+        )
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.FILE_UPLOADED_SUCCESS.value
+            }
+    )
+    
